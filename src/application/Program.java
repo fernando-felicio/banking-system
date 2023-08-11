452c222b30c4eca6ae6f41b7ca32bdb315986dae
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-11 19:32:24 -0300

    Chamando o metodo showResult

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,15 +17,24 @@ public class Program {
 		
 		System.out.println("Enter account number:");
 		holder.accNumber = sc.nextInt();
+		sc.nextLine();
 		
 		System.out.println("Enter account holder:");
 		holder.accHolder = sc.nextLine();
 		
-		//Consumindo a próxima linha para evitar o crash
-		sc.nextLine();
 		
 		System.out.println("Is there an initial deposit? (y/n)");
 		String initialDeposit = sc.nextLine();
+		
+		if (initialDeposit.equals("y")) {
+			System.out.println("Enter initial deposit value:");
+			holder.accBalance = sc.nextDouble();
+			holder.showResult();
+		}else {
+			holder.showResult();
+		}
+		
+		
 		
 		sc.close();
 		
--- src/entities/HolderAccount.java
+++ src/entities/HolderAccount.java
@@ -5,5 +5,12 @@ public class HolderAccount {
 	public int accNumber;
 	public String accHolder;
 	public double accBalance;
+	
+	public void showResult() {
+		System.out.println("Account Data:");
+		System.out.printf("Account %d, Holder: %s, Balance: R$%.2f", accNumber, accHolder, accBalance);
+	}
+	
+	
 
 }
